ea833bd0a521a18adff71f70454cfa4f34022a85
zmy <344947592@qq.com>
2019-01-24 16:43:25 +0800

    update

--- docs/README.md
+++ docs/README.md
@@ -5,17 +5,3 @@ actionText: come on →
 actionLink: guide.html
 footer: ZMY' Blog | Copyright © 2018-present Evan You
 ---
-
-:tada: :100:
-
-::: tip 提示
-this is a tip
-:::
-
-::: warning 注意
-this is a tip
-:::
-
-::: danger 警告
-this is a tip
-:::
--- docs/vuepress/config.js
+++ docs/vuepress/config.js
@@ -13,7 +13,7 @@ module.exports = {
     nav:[ // 导航栏配置
       {text: '前端基础', link: '/accumulate/' },
       {text: '算法题库', link: '/algorithm/'},
-      {text: '微博', link: 'https://baidu.com'}      
+      {text: '微博', link: 'https://baidu.com'}   
     ],
     sidebar: 'auto', // 侧边栏配置
     sidebarDepth: 2, // 侧边栏显示2级
